ec6a879bd06e56fd817fe84344c2b681d074d564
Oleg <dobrunja90@yahoo.ca>
2022-05-25 22:01:03 -0400

    fixed minor issues

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,56 +1,66 @@
+console.log("hello from service worker");
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/assets/icons/icon-192x192.png',
-    '/assets/icons/icon-512x512.png',
-    '/index.js',
-    '/indexedDB.js',
-    '/manifest.webmanifest',
-    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./js/idb.js",
+  "./js/index.js"
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+// Respond with cached resources
+self.addEventListener("fetch", function(e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
 
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  );
+});
 
-self.addEventListener('install', function(event) {
-    console.log('used to register the service worker');
-    event.waitUntil(
-        caches.open(PRECACHE)
-          .then(function(cache) {
-            return cache.addAll(FILES_TO_CACHE)
-          })
-          .then(self.skipWaiting())
-      )
-})
-  
-self.addEventListener('fetch', function(event) {
-    console.log('used to intercept requests so we can check for the file or data in the cache');
-    event.respondWith(
-        fetch(event.request)
-          .catch(() => {
-            return caches.open(PRECACHE)
-              .then((cache) => {
-                return cache.match(event.request)
-              })
-          })
-    )
-})
-  
-self.addEventListener('activate', function(event) {
-    console.log('this event triggers when the service worker activates');
-    event.waitUntil(
-        caches.keys()
-          .then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-              if (key !== PRECACHE) {
-                console.log('[ServiceWorker] Removing old cache', key)
-                return caches.delete(key)
-              }
-            }))
-          })
-          .then(() => self.clients.claim())
-    )
-})
+// Cache resources
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Delete outdated caches
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
